133510c49ef7bfaa65d1e6099a290c0624070b2d
cowdy71jake@gmail.com <>
2018-11-02 16:44:35 -0400

    Merge origin/master
    Conflicts:
    	src/githubce4/GithubCE4.java

--- src/ce4/CE4.java
+++ src/ce4/CE4.java
@@ -10,5 +10,9 @@ public class CE4
     public static void main(String[] args) 
     {
         System.out.println("Insert good programming here.");
+        
+        for(int i = 0; i < 1000; i++){
+            System.out.print("lol");
+        }
     }
 }
